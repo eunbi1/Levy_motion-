c3d4ffc162f8c8555a70e8014a52faf26f5c4078
eunbi1 <82250837+eunbi1@users.noreply.github.com>
2022-09-08 19:44:54 +0900

    Update training.py

--- training.py
+++ training.py
@@ -31,7 +31,7 @@ channels = 1
 batch_size = 128
 
 
-def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,
+def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,training_clamp=3,
           n_epochs=15,num_steps=1000, datasets ='MNIST',path = None, device='cuda'):
     sde = VPSDE(alpha, beta_min = beta_min, beta_max = beta_max, device=device)
 
@@ -63,6 +63,7 @@ def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,
         dataset = CelebA('/scratch/private/eunbiyoon/Levy_motion-', transform=transform, download=True)
         data_loader = DataLoader(dataset, batch_size=batch_size,
                              shuffle=True, num_workers=num_workers,generator=torch.Generator(device=device))
+        
 
         score_model = Model(resolution=image_size, in_channels=channels,out_ch=channels)
 
@@ -72,6 +73,13 @@ def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,
                              shuffle=True, num_workers=num_workers,generator=torch.Generator(device=device))
         image_size = 32
         channels = 3
+        transformer = transforms.Compose([transforms.Resize((32, 32)),
+                                          transforms.ToTensor()
+                                          ])
+        validation_dataset = CIFAR10(root='/scratch/private/eunbiyoon/data', train=False, download=True,
+                                     transform=transformer)
+        validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=False)
+
         score_model = Model()
 
 
@@ -82,10 +90,10 @@ def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,
 
     optimizer = Adam(score_model.parameters(), lr=lr)
 
-
-    L = []
     counter = 0
     t0 = time.time()
+    L = []
+    L_val = []
     for epoch in range(n_epochs):
         counter += 1
         avg_loss = 0.
@@ -94,7 +102,7 @@ def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,
         for x,y in data_loader:
             x = 2*x - 1
             x = x.to(device)
-            e_L = torch.clamp(levy.sample(alpha, 0, size=x.shape ).to(device),-10,10)
+            e_L = torch.clamp(levy.sample(alpha, 0, size=x.shape ).to(device),-training_clamp,training_clamp)
 
             t = torch.rand(x.shape[0]).to(device)+1e-4
             loss = loss_fn(score_model, sde, x, t, e_L, num_steps=num_steps)
@@ -106,16 +114,54 @@ def train(alpha=2, lr = 1e-4, batch_size=128, beta_min=0.1, beta_max = 20,
             optimizer.step()
             avg_loss += loss.item() * x.shape[0]
             num_items += x.shape[0]
-        t1 = time.time()
-        L.append(avg_loss / num_items)
-        print(f'{epoch} th epoch loss: {avg_loss / num_items}')
-        print('Running time:', t1-t0)
-        ckpt_name = str(datasets)+ str(f'{alpha}_{beta_min}_{beta_max}.pth')
-        ckpt_name=os.path.join('/scratch/private/eunbiyoon/Levy_motion-', ckpt_name)
-        torch.save(score_model.state_dict(),ckpt_name)
-    name = str(datasets)+str(time.strftime('%m%d_%H%M_', time.localtime(time.time()))) +'_'+ 'alpha'+str(f'{alpha}')+ 'beta'+str(f'{beta_min}')+ '_'+ str(
-            f'{beta_max}') + '.pth'
-    dir_path = os.path.join('/scratch/private/eunbiyoon/Levy_motion-', 'checkpoint')
+        else:
+            with torch.no_grad():
+                counter += 1
+                val_avg_loss = 0.
+                val_num_items = 0
+                for x, y in validation_loader:
+                    x= 2*x-1
+                    x = x.to(device)
+                    t = torch.rand(x.shape[0]).to(device) + 1e-4
+                    e_L = torch.clamp(levy.sample(alpha, 0, size=x.shape).to(device), -training_clamp, training_clamp)
 
-    if not os.path.isdir(dir_path):
-            os.mkdir
+                    val_loss = loss_fn(score_model, sde, x, t, e_L, num_steps=num_steps)
+                    val_avg_loss += val_loss.item() * x.shape[0]
+                    val_num_items += x.shape[0]
+        L.append(avg_loss / num_items)
+        L_val.append(val_avg_loss / val_num_items)
+        t1 = time.time()
+        print(f'{epoch+initial_epoch} th epoch loss: {avg_loss / num_items}')
+        print(f'{epoch+initial_epoch} th epoch validation loss: {val_avg_loss / val_num_items}')
+        print('Running time:', t1 - t0)
+
+        ckpt_name =str(datasets) + str(
+            f'batch{batch_size}') + str(f'{alpha}_{beta_min}_{beta_max}.pth')
+        dir_path = str(datasets) + str(
+            f'clamp{training_clamp}batch{batch_size}lr{lr}') + str(f'{alpha}_{beta_min}_{beta_max}')
+        dir_path = os.path.join('/scratch/private/eunbiyoon/Levy_motion-', dir_path)
+        if not os.path.isdir(dir_path):
+            os.mkdir(dir_path)
+
+        dir_path2 = os.path.join(dir_path, 'ckpt')
+        if not os.path.isdir(dir_path2):
+            os.mkdir(dir_path2)
+
+        ckpt_name = os.path.join(dir_path2, ckpt_name)
+        torch.save(score_model.state_dict(), ckpt_name)
+
+        plt.plot(L, color='r', label='training loss')
+        plt.plot(L_val, color='b', label='validation loss')
+
+        plt.axis('on')
+        name_ = os.path.join(dir_path, 'loss.png')
+        plt.savefig(name_)
+
+        sample(alpha=1.9, path=ckpt_name,
+               beta_min=beta_min, beta_max=beta_max, sampler='pc_sampler2', batch_size=64, num_steps=1000, LM_steps=50,
+               Predictor=True, Corrector=False, trajectory=False, clamp=training_clamp, initial_clamp=training_clamp,
+               clamp_mode="constant", 
+               datasets=datasets, name=str(str(f'epoch{epoch}') ),
+               dir_path=dir_path)
+        plt.cla()
+        plt.close()
